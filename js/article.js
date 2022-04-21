ab20fc83125ffa3d15f4b536b3129f33fb60e967
Boboev <92302413+MuhamadBoboev@users.noreply.github.com>
2022-04-21 19:45:54 +0300

    add article section

---
+++ js/article.js
@@ -0,0 +1,19 @@
+const imgArticleGridContainer = document.querySelector('.article__grid');
+const imgBig = document.querySelector('[data-article]').src;
+document.querySelectorAll('.articleImg').forEach( function (event) {
+  event.addEventListener('mouseover', function(){
+    const imgSmall = event.querySelector('[data-article]').src;
+    const imgSmall1 = event.querySelector('[data-article]').src;
+    const imgBig = document.querySelector('[data-article]').src;
+
+    document.querySelector('[data-article]').src = imgSmall;
+
+    this.querySelector('[data-article]').src = imgBig
+    console.log()
+    })
+  })
+// document.querySelectorAll('.articleImg').forEach( function(event){
+//   event.addEventListener('mousedown', function(){
+//     event.classList.toggle('articleImg-hover')
+//   })
+// })
